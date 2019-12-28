8dc8fafcc366e7244208e80cda95872fba97cab9
Noam Sauer-Utley <noamsauerutley@gmail.com>
2019-12-28 17:47:08 -0500

    Add image C/D

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -6,8 +6,8 @@ import { set_current_character, delete_character } from '../redux/actions'
 import { NavLink} from 'react-router-dom'
 
 class CharacterCard extends React.Component{
-     character = this.props.character
-     currentId = this.character.id
+    //  character = this.props.character
+    //  currentId = this.character.id
 
      setCurrentCharacter = () => {
         this.props.set_current_character(this.props.character)
@@ -15,21 +15,21 @@ class CharacterCard extends React.Component{
 
 
      delete = async () => {
-        this.props.delete_character(this.currentId)
-        await fetch(`http://localhost:3000/characters/${this.currentId}`, {
+        this.props.delete_character(this.props.character.id)
+        await fetch(`http://localhost:3000/characters/${this.props.character.id}`, {
             method: 'DELETE',
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": localStorage.token
             }
         })
-        console.log(this.currentId)
-        console.log(this.character, "deleted!")
+        console.log(this.props.character.id)
+        console.log(this.props.character, "deleted!")
     } 
 
     render(){
         return <li style={{listStyle: "none"}}>
-            <h5>{this.character.name}</h5>
+            <h5>{this.props.character.name}</h5>
             <NavLink to="/characters/edit" style={{color: "black", textDecoration: "none"}} onClick={this.setCurrentCharacter}>✎</NavLink><br></br>
             <ul style={{
                 width: "100%",
@@ -40,12 +40,12 @@ class CharacterCard extends React.Component{
                 textAlign: "center",
                 justifyContent: "space-around"
             }}>
-                {!!this.character.images ? this.character.images.map(image => <ImageCard image={image} />) : ""}</ul>
+                {!!this.props.character.images ? this.props.character.images.map(image => <ImageCard image={image} />) : ""}</ul>
                 <NavLink to={`/images/new`} style={{marginTop: "20px", color: "black", textDecorationColor: "black"}} onClick={this.setCurrentCharacter}>ADD IMAGE</NavLink>
-                <p>{this.character.description}</p>
+                <p>{this.props.character.description}</p>
                 <label>NOTES:</label>
-            <ul>{!!this.character.character_notes ? this.character.character_notes.map(character_note => < CharacterNoteCard character_note={character_note}/>) : "You haven't added any notes to this character."}</ul>
-            <NavLink to={`/stories/${this.character.story_id}`} onClick={this.delete} style={{color: "black"}}>DELETE</NavLink>
+            <ul>{!!this.props.character.character_notes ? this.props.character.character_notes.map(character_note => < CharacterNoteCard character_note={character_note}/>) : "You haven't added any notes to this character."}</ul>
+            <NavLink to={`/stories/${this.props.character.story_id}`} onClick={this.delete} style={{color: "black"}}>DELETE</NavLink>
 
         </li>
     }
